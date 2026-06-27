53ac0abad185cf00beae879611449d55921a236b
tannutannudev-tech <tannu.tannu.dev@gmail.com>
2026-06-27 17:43:53 +0530

    js

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
-let currentPlayer = "X";
-
-const boxes = document.querySelectorAll(".cell");
-const turn = document.querySelector(".turn");
-
-const winningPatterns = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
-
-boxes.forEach((box) => {
-
-    box.addEventListener("click", function(){
-
-        if(box.innerHTML === ""){
-
-            box.innerHTML = currentPlayer;
-
-            checkWinner();
-
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-
-            turn.innerHTML = "Current Player : " + currentPlayer;
+let cp = document.querySelectorAll(".cell-a");
+let currentplayer = "X";
+let turn = document.querySelector(".turn");
+let gameover = false;
+cp.forEach((cell) => {
+    cell.addEventListener("click", () => {
+        if (gameover) {
+            return;
         }
+        if (cell.innerHTML === "") {
+            if (currentplayer === "X") {
+                cell.innerHTML = "X";
+                currentplayer = "O";
+            }
+            else {
+                cell.innerHTML = "O";
+                currentplayer = "X";
+            }
 
+            turn.innerHTML = " Current Player : " + currentplayer;
+            console.log(turn);
+            winnerpattern();
+
+        }
+        function winnerpattern() {
+            let arr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+            arr.forEach((pattern) => {
+                let pos1 = cp[pattern[0]];
+                let pos2 = cp[pattern[1]];
+                let pos3 = cp[pattern[2]];
+                if (pos1.innerText !== "" && pos1.innerText === pos2.innerText && pos2.innerText === pos3.innerText) {
+                    turn.innerHTML = "winner is : " + pos1.innerText;
+                    //alert("winner is : " + pos1.innerText)
+                    pos1.classList.add("winner");
+                    pos2.classList.add("winner");
+                    pos3.classList.add("winner");
+                    gameover = true;
+                }
+            });
+        };
     });
-
 });
-
-function checkWinner(){
-
-    winningPatterns.forEach((pattern)=>{
-
-        let pos1 = boxes[pattern[0]].innerHTML;
-        let pos2 = boxes[pattern[1]].innerHTML;
-        let pos3 = boxes[pattern[2]].innerHTML;
-
-        if(
-            pos1 !== "" &&
-            pos1 === pos2 &&
-            pos2 === pos3
-        ){
-            alert("Winner is " + pos1);
-        }
-
-    });
-
-}
